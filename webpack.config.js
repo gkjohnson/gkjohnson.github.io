31f2bfb71b1ba56cf8e6c5241fb716def8a9bc74
Garrett Johnson <garrett.kjohnson@gmail.com>
2017-08-21 21:42:20 -0700

    Prepare for merge to master and get in-progress work into a separate index.html

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: __dirname + '/index-inprogress.html',
+    output: {
+        path: __dirname ,
+        filename: 'index.bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [{ loader: 'wc-loader' }]
+            },
+            {
+                test: /\.js$/,
+                use: [{ loader: 'script-loader' }]
+            },
+            {
+                test: /\.jpeg$/,
+                use: [{loader: 'url-loader'}]
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({ filename: 'index-inprogress.bundle.html' })
+    ]
+}
